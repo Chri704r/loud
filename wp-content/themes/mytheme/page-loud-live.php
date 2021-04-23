4de91774145bbc17d8e449bbe4a9fa93a8d1cb20
nico755e <nico755e@stud.kea.dk>
2021-04-23 09:48:27 +0200

    buttons

--- wp-content/themes/mytheme/page-loud-live.php
+++ wp-content/themes/mytheme/page-loud-live.php
@@ -1,13 +1,25 @@
 <?php mesmerize_get_header(); ?>
 
-    <div id='page-content' class="page-content">
-        <div class="<?php mesmerize_page_content_wrapper_class(); ?>">
-            <?php
+<div id='page-content' class="page-content">
+	<div class="<?php mesmerize_page_content_wrapper_class(); ?>">
+		<?php
             while (have_posts()) : the_post();
                 get_template_part('template-parts/content', 'front');
             endwhile;
             ?>
-        </div>
-    </div>
+	</div>
 
-    <?php get_footer(); ?>
+	<div class="ugedage">
+		<button id="mandag"></button>
+		<button id="tirsdag"></button>
+		<button id="onsdag"></button>
+		<button id="torsdag"></button>
+		<button id="fredag"></button>
+		<button id="lordag"></button>
+		<button id="sondag"></button>
+	</div>
+
+
+</div>
+
+<?php get_footer(); ?>
