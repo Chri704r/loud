2f55211d98051af9fe0f4f6cc63c1d8d590922fa
Christine Piilmann <christinepiilmann@Christines-MacBook-Air.local>
2021-04-23 09:30:03 +0200

    forældre kategori tilføjet

--- wp-content/themes/mytheme/page-podcasts.php
+++ wp-content/themes/mytheme/page-podcasts.php
@@ -3,7 +3,8 @@
     <div id='page-content' class="page-content">
 
         <h2>Alle podcasts</h2>
-        <section class="alle_podcasts"></section>
+        <section class="alle_podcasts">
+        </section>
 
         <h2>Kategorier</h2>
         <section class="kategori"></section>
@@ -26,6 +27,11 @@
                 width: 100%;
             }
 
+            .alle {
+                width: 100%;
+                background-color: #b2b2b2;
+            }
+
             .alle_podcasts {
                 display: grid;
                 grid-template-columns: repeat(6, 1fr);
@@ -36,10 +42,22 @@
                 display: none;
             }
 
+            .alle_podcasts:after {
+                content: "Se alle";
+                display: inline;
+                background-color: grey;
+                width: 100%;
+                color: #2a2a2a;
+                text-align: center;
+                vertical-align: middle;
+                padding-top: 43%;
+                font-size: 1.5em;
+            }
+
             .kategori {
                 display: grid;
-                grid-template-columns: repeat(6, 1fr);
-                grid-gap: 20px;
+                grid-template-columns: repeat( auto-fill, minmax(250px, 1fr));
+                grid-gap: 10px;
             }
 
             .kategori:before {
@@ -47,8 +65,8 @@
             }
 
             .page-content button {
-                width: 150px;
-                height: 150px;
+                width: 250px;
+                height: 250px;
                 border: none;
                 border-radius: 10px;
                 background-color: #c9c9c9;
@@ -63,7 +81,7 @@
             let categories;
 
             const dbUrl = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/podcast?per_page=11";
-            const catUrl = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/categories?per_page=100";
+            const catUrl = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/categories?parent=22";
 
             async function getJson() {
                 const data = await fetch(dbUrl);
