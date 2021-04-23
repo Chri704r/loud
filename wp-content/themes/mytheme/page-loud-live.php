39673c40e499657680af22f9abd9b3b114014bc9
nico755e <nico755e@stud.kea.dk>
2021-04-23 14:09:50 +0200

    consolelog test

--- wp-content/themes/mytheme/page-loud-live.php
+++ wp-content/themes/mytheme/page-loud-live.php
@@ -37,6 +37,7 @@
 
 <?php mesmerize_get_header(); ?>
 
+<!--
 <div id='page-content' class="page-content">
 	<div class="<?php mesmerize_page_content_wrapper_class(); ?>">
 		<?php
@@ -45,99 +46,115 @@
             endwhile;
             ?>
 	</div>
+-->
 
-	<main>
+<main>
 
-		<div class="ugedage">
-			<button id="mandag">
-				<p>Mandag</p>
-			</button>
-			<button id="tirsdag">
-				<p>Tirsdag</p>
-			</button>
-			<button id="onsdag">
-				<p>Onsdag</p>
-			</button>
-			<button id="torsdag">
-				<p>Torsdag</p>
-			</button>
-			<button id="fredag">
-				<p>Fredag</p>
-			</button>
-			<button id="lordag">
-				<p>Lørdag</p>
-			</button>
-			<button id="sondag">
-				<p>Søndag</p>
-			</button>
-		</div>
-
-		<button id="live">
-			<p>AFSPIL LIVE</p>
+	<div class="ugedage">
+		<button id="mandag">
+			<p>Mandag</p>
 		</button>
+		<button id="tirsdag">
+			<p>Tirsdag</p>
+		</button>
+		<button id="onsdag">
+			<p>Onsdag</p>
+		</button>
+		<button id="torsdag">
+			<p>Torsdag</p>
+		</button>
+		<button id="fredag">
+			<p>Fredag</p>
+		</button>
+		<button id="lordag">
+			<p>Lørdag</p>
+		</button>
+		<button id="sondag">
+			<p>Søndag</p>
+		</button>
+	</div>
+
+	<button id="live">
+		<p>AFSPIL LIVE</p>
+	</button>
 
 
 
-		<section class="container"></section>
+	<section class="container"></section>
 
 
 
-		<template>
-			<article>
-				<div class="tid">
-					<p></p>
-				</div>
+	<template>
+		<article>
+			<div class="tid">
+				<p></p>
+			</div>
 
-				<div class="billede">
-					<img src="" alt="">
-				</div>
+			<div class="billede">
+				<img src="" alt="">
+			</div>
 
-				<div class="beskrivelse">
-					<h3></h3>
-					<p></p>
-				</div>
-			</article>
-		</template>
-
-
+			<div class="beskrivelse">
+				<h3></h3>
+				<p></p>
+			</div>
+		</article>
+	</template>
 
 
 
-</div>
+
+
+	</div>
 
 
 
-<?php get_footer(); ?>
+	<?php get_footer(); ?>
 
 
-<!------- SCRIPT BEGYNDER ------->
-<script>
-	let podcasts = [];
-	const liste = document.querySelector(".container");
-	const skabelon = document.querySelector("template");
-	let filterPodcast = "alle";
-	document.addEventListener("DOMContentLoaded", start);
+	<!------- SCRIPT BEGYNDER ------->
+	<script>
+		let podcasts;
 
-	function start() {
-		getJson();
-	}
+		const dbUrl = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/podcast?per_page=100";
 
-	const url = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/podcast?per_page=100";
-	async function getJson() {
-		let response = awai fetch(url);
-		podcasts = await response.json();
-		visPodcasts();
-	}
 
-	function visPodcasts() {
-		console.log(podcasts);
-		podcasts.forEach(podcast => {
-			const klon = skabelon.cloneNode(true).content;
-			klon.querySelector("img").src = podcast.billede.guid;
-			klon.querySelector("h3").textContent = podcast.title.rendered;
-			klon.querySelector("p").textContent = podcast.podcast_beskrivelse;
-			liste.appendChild(klon);
-		})
-	}
+		async function getJson() {
+			const data = await fetch(dbUrl);
+			podcasts = await data.json();
+			console.log(podcasts);
+		}
 
-</script>
+
+
+
+
+		//	let podcasts = [];
+		//	const liste = document.querySelector(".container");
+		//	const skabelon = document.querySelector("template");
+		//	let filterPodcast = "alle";
+		//	document.addEventListener("DOMContentLoaded", start);
+		//
+		//	function start() {
+		//		getJson();
+		//	}
+		//
+		//	const url = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/podcast?per_page=100";
+		//	async function getJson() {
+		//		let response = awai fetch(url);
+		//		podcasts = await response.json();
+		//		visPodcasts();
+		//	}
+		//
+		//	function visPodcasts() {
+		//		console.log(podcasts);
+		//		podcasts.forEach(podcast => {
+		//			const klon = skabelon.cloneNode(true).content;
+		//			klon.querySelector("img").src = podcast.billede.guid;
+		//			klon.querySelector("h3").textContent = podcast.title.rendered;
+		//			klon.querySelector("p").textContent = podcast.podcast_beskrivelse;
+		//			liste.appendChild(klon);
+		//		})
+		//	}
+
+	</script>
