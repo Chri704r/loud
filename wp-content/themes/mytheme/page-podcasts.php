a64b24074c0c4fc48609d1396f7c93b206d30ef5
Christine Piilmann <christinepiilmann@Christines-MacBook-Air.local>
2021-04-23 17:04:26 +0200

    single, front og podcasts side ændringer

--- wp-content/themes/mytheme/page-podcasts.php
+++ wp-content/themes/mytheme/page-podcasts.php
@@ -1,17 +1,18 @@
 <?php mesmerize_get_header(); ?>
 
     <div id='page-content' class="page-content">
+        <div class="main">
 
-        <h2>Alle podcasts</h2>
-        <section class="alle_podcasts">
-        </section>
+            <h2>Alle podcasts</h2>
+            <section class="alle_podcasts">
+            </section>
 
-        <h2>Kategorier</h2>
-        <section class="kategori"></section>
+            <h2>Kategorier</h2>
+            <section class="kategori"></section>
 
-        <h2>Til det du laver</h2>
-        <section class="laver"></section>
-
+            <h2>Til det du laver</h2>
+            <section class="laver"></section>
+        </div>
     </div>
 
     <template>
@@ -27,6 +28,26 @@
                 width: 100%;
             }
 
+            h2 {
+                margin-top: 40px;
+                font-size: 1.8em;
+                line-height: 0.1em;
+                margin-bottom: 25px;
+            }
+
+            .billede {
+                border-radius: 20px;
+            }
+
+            .mesmerize-inner-page #page .page-content {
+                padding-top: 0em;
+            }
+
+            .main {
+                max-width: 1200px;
+                margin: 0 auto;
+            }
+
             .alle {
                 width: 100%;
                 background-color: #b2b2b2;
@@ -35,7 +56,7 @@
             .alle_podcasts {
                 display: grid;
                 grid-template-columns: repeat(6, 1fr);
-                grid-gap: 20px;
+                grid-gap: 15px;
             }
 
             .alle_podcasts:before {
@@ -45,31 +66,76 @@
             .alle_podcasts:after {
                 content: "Se alle";
                 display: inline;
-                background-color: grey;
+                background-color: #e4254a;
                 width: 100%;
-                color: #2a2a2a;
+                color: white;
                 text-align: center;
                 vertical-align: middle;
                 padding-top: 43%;
                 font-size: 1.5em;
+                border-radius: 20px;
             }
 
             .kategori {
                 display: grid;
-                grid-template-columns: repeat( auto-fill, minmax(250px, 1fr));
-                grid-gap: 10px;
+                grid-template-columns: repeat(5, 1fr);
+                grid-auto-rows: 1fr;
+                grid-gap: 20px;
             }
 
             .kategori:before {
                 display: none;
             }
 
+            .laver {
+                display: grid;
+                grid-template-columns: repeat(5, 1fr);
+                grid-auto-rows: 1fr;
+                grid-gap: 20px;
+            }
+
+            .laver:before {
+                display: none;
+            }
+
             .page-content button {
-                width: 250px;
-                height: 250px;
+                width: 100%;
+                height: 100%;
+                /*                height: 200px;*/
                 border: none;
-                border-radius: 10px;
-                background-color: #c9c9c9;
+                border-radius: 30px;
+                background-color: #e5e5e5;
+                font-weight: 600;
+                font-size: 15px;
+            }
+
+            button.filter {
+                display: grid;
+                grid-template-rows: 80%;
+            }
+
+            button img {
+                width: 60%;
+                margin-top: 15%;
+                margin-left: auto;
+                margin-right: auto;
+                padding-bottom: 45px;
+            }
+
+            [data-queen="4"] {
+                padding-top: 40%;
+                font-size: 30px !important;
+            }
+
+            @media only screen and (max-width: 700px) {
+                .alle_podcasts {
+                    display: flex;
+                    flex-direction: row;
+                    overflow-x: scroll;
+                }
+                .billede {
+                    width: 70%;
+                }
             }
 
         </style>
@@ -79,32 +145,75 @@
         <script>
             let podcasts;
             let categories;
+            let categories2;
 
             const dbUrl = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/podcast?per_page=11";
             const catUrl = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/categories?parent=22";
+            const catUrl2 = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/categories?parent=23";
 
             async function getJson() {
                 const data = await fetch(dbUrl);
                 const catdata = await fetch(catUrl);
+                const catdata2 = await fetch(catUrl2);
+
                 podcasts = await data.json();
                 categories = await catdata.json();
+                categories2 = await catdata2.json();
+
                 console.log(categories);
                 console.log(podcasts);
+                console.log(categories2);
 
                 visPodcasts();
                 opretKnapper();
+                opretKnapper2();
 
             }
 
             function opretKnapper() {
 
                 categories.forEach(cat => {
-                    document.querySelector(".kategori").innerHTML += `<button class="filter" data-queen="${cat.id}">${cat.name}</button>`
+                    document.querySelector(".kategori").innerHTML += `<button class="filter" data-queen="${cat.id}">${cat.description}${cat.name}</button>`
                 })
 
                 addEventListernesToButtons();
 
             }
+
+            function opretKnapper2() {
+
+                categories2.forEach(cat => {
+                    document.querySelector(".laver").innerHTML += `<button class="filter" data-queen="${cat.id}">${cat.description}${cat.name}</button>`
+                })
+
+                addEventListernesToButtons();
+
+            }
+
+            function addEventListernesToButtons() {
+                document.querySelectorAll(".main button").forEach(elm => {
+                    elm.addEventListener("click", filtrering);
+
+                })
+
+            };
+
+            function filtrering() {
+
+                console.log("click");
+                //                            filterQueen = this.dataset.queen;
+                //location.href = `archive.php?id=${}`;
+
+            }
+            //
+            //            function filtrering() {
+            //                klon.querySelector("article").addEventListener("click", () => {
+            //                    location.href = cat.link;
+            //                })
+            //            }
+
+
+            //            location.href = `detail.html?id=${hvem._id}`;
 
 
             //---------INDSÆTTER PODCASTS I KLON-------
@@ -116,9 +225,15 @@
 
                     const klon = template.cloneNode(true);
                     klon.querySelector(".billede").src = podcast.billede.guid;
+                    klon.querySelector(".billede").addEventListener("click", () => visDetaljer(podcast));
 
                     container.appendChild(klon);
                 })
+
+            }
+
+            function visDetaljer(hvem) {
+                location.href = hvem.link;
 
             }
 
--- wp-content/themes/mytheme/single-podcast.php
+++ wp-content/themes/mytheme/single-podcast.php
@@ -1,78 +1,78 @@
 <?php mesmerize_get_header(); ?>
-<button>Tilbage</button>
-<h1 class="podcast_navn">Podcasten</h1>
-<article id="podcast">
-	<img src="" alt="" class="podcast_billede">
-	<p class="beskrivelse"></p>
-</article>
+    <button>Tilbage</button>
+    <h1 class="podcast_navn">Podcasten</h1>
+    <article id="podcast">
+        <img src="" alt="" class="podcast_billede">
+        <p class="beskrivelse"></p>
+    </article>
 
-<article id="ny_podcast">
-	<h2>Nyeste episode</h2>
-	<div class="nyeste_container">
-		<div class="col1"><img src="" alt="" class="nyeste_billede"></div>
-	</div>
-	<div class="col2">
-		<h3 class="ny_episode">Episode navn</h3>
-		<p class="ny_tekst"></p>
-	</div>
-</article>
+    <article id="ny_podcast">
+        <h2>Nyeste episode</h2>
+        <div class="nyeste_container">
+            <div class="col1"><img src="" alt="" class="nyeste_billede"></div>
+        </div>
+        <div class="col2">
+            <h3 class="ny_episode">Episode navn</h3>
+            <p class="ny_tekst"></p>
+        </div>
+    </article>
 
-<h2>Alle Episoder</h2>
-<section class="episoder">
-	<template>
-		<img src="" alt="" class="podcast_billede">
-		<div>
-			<h3 class="episode_navn"></h3>
-			<p class="episode_tekst"></p>
-			<a href="">Læs mere</a>
-		</div>
-	</template>
-</section>
+    <h2>Alle Episoder</h2>
+    <section class="episoder">
+        <template>
+            <img src="" alt="" class="podcast_billede">
+            <div>
+                <h3 class="episode_navn"></h3>
+                <p class="episode_tekst"></p>
+                <a href="">Læs mere</a>
+            </div>
+        </template>
+    </section>
 
-<script>
-	document.addEventListener("DOMContentLoaded", getJson);
-	let podcast;
-	let episoder;
-	let aktuelPodcast = <?php echo get_the_ID() ?>;
+    <script>
+        document.addEventListener("DOMContentLoaded", getJson);
+        let podcast;
+        let episoder;
+        let aktuelPodcast = <?php echo get_the_ID() ?>;
 
-	const dbUrl = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/podcast/" + aktuelPodcast;
-	const episodeUrl = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/episode/";
+        const dbUrl = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/podcast/" + aktuelPodcast;
+        const episodeUrl = "http://piilmanndesigns.dk/kea/09_cms/loud/wp-json/wp/v2/episode/";
 
-	async function getJson() {
-		const data = await fetch(dbUrl);
-		podcast = await data.json();
+        async function getJson() {
+            const data = await fetch(dbUrl);
+            podcast = await data.json();
 
-		const data2 = await fetch(dbUrl);
-		episoder = await data2.json();
-
-
-		visPodcast();
-		visEpisoder();
-	}
-
-	function visPodcast() {
-		console.log("visPodcasts");
-		document.querySelector(".podcast_billede").src = podcast.picture.guid;
-		document.querySelector(".podcast_billede").alt = podcast.title.rendered;
-		document.querySelector(".podcast_billede").title = podcast.title.rendered;
-
-		document.querySelector("h1").innerHTML = podcast.title.rendered;
-
-		document.querySelector(".ny_episode").innerHTML = episode.title.rendered;
-		document.querySelector(".episode_navn").innerHTML = episode.title.rendered;
+            const data2 = await fetch(dbUrl);
+            episoder = await data2.json();
 
 
-		document.querySelector(".beskrivelse").innerHTML = podcast.beskrivelse;
+            visPodcast();
+            visEpisoder();
+        }
 
-		document.querySelector(".episode_tekst").innerHTML = podcast.;
+        function visPodcast() {
+            console.log("visPodcasts");
+            document.querySelector(".podcast_billede").src = podcast.billede.guid;
+            document.querySelector(".podcast_billede").alt = podcast.title.rendered;
+            document.querySelector(".podcast_billede").title = podcast.title.rendered;
+
+            document.querySelector("h1").innerHTML = podcast.title.rendered;
+
+            document.querySelector(".ny_episode").innerHTML = episode.title.rendered;
+            document.querySelector(".episode_navn").innerHTML = episode.title.rendered;
 
 
-		document.querySelector("button").addEventListener("click", tilbageTilMenu);
-	}
+            document.querySelector(".beskrivelse").innerHTML = podcast.beskrivelse;
 
-	function tilbageTilMenu() {
-		history.back();
-	}
+            document.querySelector(".episode_tekst").innerHTML = podcast.;
 
-</script>
-<?php get_footer(); ?>
+
+            document.querySelector("button").addEventListener("click", tilbageTilMenu);
+        }
+
+        function tilbageTilMenu() {
+            history.back();
+        }
+
+    </script>
+    <?php get_footer(); ?>
