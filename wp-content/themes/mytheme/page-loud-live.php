c8d28a096dcf1e5b72b91299279eed321e72e65c
nico755e <nico755e@stud.kea.dk>
2021-04-23 10:10:06 +0200

    grid columns prøve

--- wp-content/themes/mytheme/page-loud-live.php
+++ wp-content/themes/mytheme/page-loud-live.php
@@ -1,3 +1,13 @@
+<style>
+	.liste {
+		display: grid;
+		grid-auto-columns: 40% 40% 20%;
+	}
+
+</style>
+
+
+
 <?php mesmerize_get_header(); ?>
 
 <div id='page-content' class="page-content">
@@ -10,16 +20,100 @@
 	</div>
 
 	<div class="ugedage">
-		<button id="mandag"></button>
-		<button id="tirsdag"></button>
-		<button id="onsdag"></button>
-		<button id="torsdag"></button>
-		<button id="fredag"></button>
-		<button id="lordag"></button>
-		<button id="sondag"></button>
+		<button id="mandag">
+			<p>Mandag</p>
+		</button>
+		<button id="tirsdag">
+			<p>Tirsdag</p>
+		</button>
+		<button id="onsdag">
+			<p>Onsdag</p>
+		</button>
+		<button id="torsdag">
+			<p>Torsdag</p>
+		</button>
+		<button id="fredag">
+			<p>Fredag</p>
+		</button>
+		<button id="lordag">
+			<p>Lørdag</p>
+		</button>
+		<button id="sondag">
+			<p>Søndag</p>
+		</button>
+	</div>
+
+	<div id="main">
+		<button id="live">
+			<p>AFSPIL LIVE</p>
+		</button>
+
+		<div class="liste">
+			<div class="tid"></div>
+			<div class="billede"></div>
+			<div class="beskrivelse"></div>
+		</div>
+
+
+		<div class="liste">
+			<div class="tid"></div>
+			<div class="billede"></div>
+			<div class="beskrivelse"></div>
+		</div>
+
+
+		<div class="liste">
+			<div class="tid"></div>
+			<div class="billede"></div>
+			<div class="beskrivelse"></div>
+		</div>
+
+
+		<div class="liste">
+			<div class="tid"></div>
+			<div class="billede"></div>
+			<div class="beskrivelse"></div>
+		</div>
+
+
+		<div class="liste">
+			<div class="tid"></div>
+			<div class="billede"></div>
+			<div class="beskrivelse"></div>
+		</div>
+
+
+		<div class="liste">
+			<div class="tid"></div>
+			<div class="billede"></div>
+			<div class="beskrivelse"></div>
+		</div>
+
+
+		<div class="liste">
+			<div class="tid"></div>
+			<div class="billede"></div>
+			<div class="beskrivelse"></div>
+		</div>
+
+
+		<div class="liste">
+			<div class="tid"></div>
+			<div class="billede"></div>
+			<div class="beskrivelse"></div>
+		</div>
+
+
 	</div>
 
 
 </div>
 
 <?php get_footer(); ?>
+
+
+<!------- SCRIPT BEGYNDER ------->
+<script>
+
+
+</script>
