9d0f469873bb12e914d85efeaa1511b914f70b48
nico755e <nico755e@stud.kea.dk>
2021-04-26 10:22:18 +0200

    tabs try

--- wp-content/themes/mytheme/page-loud-live.php
+++ wp-content/themes/mytheme/page-loud-live.php
@@ -53,13 +53,14 @@
 
 
 	/* LÅNT KODE */
+	/* Style the tab */
 	.tab {
 		overflow: hidden;
 		border: 1px solid #ccc;
 		background-color: #f1f1f1;
 	}
 
-	/* Style the buttons inside the tab */
+	/* Style the buttons that are used to open the tab content */
 	.tab button {
 		background-color: inherit;
 		float: left;
@@ -68,7 +69,6 @@
 		cursor: pointer;
 		padding: 14px 16px;
 		transition: 0.3s;
-		font-size: 17px;
 	}
 
 	/* Change background color of buttons on hover */
@@ -108,27 +108,33 @@
 
 <main>
 
+	<!-- Tab links -->
 	<div class="tab">
+		<button class="tablinks" onclick="openCity(event, 'London')">London</button>
+		<button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
+		<button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
+	</div>
 
-		<div id="London" class="tabcontent">
-			<h3>London</h3>
-			<p>London is the capital city of England.</p>
-		</div>
+	<!-- Tab content -->
+	<div id="London" class="tabcontent">
+		<h3>London</h3>
+		<p>London is the capital city of England.</p>
+	</div>
 
-		<div id="Paris" class="tabcontent">
-			<h3>Paris</h3>
-			<p>Paris is the capital of France.</p>
-		</div>
+	<div id="Paris" class="tabcontent">
+		<h3>Paris</h3>
+		<p>Paris is the capital of France.</p>
+	</div>
 
-		<div id="Tokyo" class="tabcontent">
-			<h3>Tokyo</h3>
-			<p>Tokyo is the capital of Japan.</p>
-		</div>
-
-
+	<div id="Tokyo" class="tabcontent">
+		<h3>Tokyo</h3>
+		<p>Tokyo is the capital of Japan.</p>
+	</div>
 
 
-		<!--
+
+
+	<!--
 		<button class="tablinks" onclick="openDag(event, 'London')">
 			<p>Mandag</p>
 		</button>
@@ -151,7 +157,7 @@
 			<p>Søndag</p>
 		</button>
 -->
-	</div>
+
 
 	<button id="live">
 		<p>AFSPIL LIVE</p>
@@ -220,16 +226,29 @@
 
 		getJson();
 
+
+
+
+
+
+
 		function openCity(evt, cityName) {
+			// Declare all variables
 			var i, tabcontent, tablinks;
+
+			// Get all elements with class="tabcontent" and hide them
 			tabcontent = document.getElementsByClassName("tabcontent");
 			for (i = 0; i < tabcontent.length; i++) {
 				tabcontent[i].style.display = "none";
 			}
+
+			// Get all elements with class="tablinks" and remove the class "active"
 			tablinks = document.getElementsByClassName("tablinks");
 			for (i = 0; i < tablinks.length; i++) {
 				tablinks[i].className = tablinks[i].className.replace(" active", "");
 			}
+
+			// Show the current tab, and add an "active" class to the button that opened the tab
 			document.getElementById(cityName).style.display = "block";
 			evt.currentTarget.className += " active";
 		}
