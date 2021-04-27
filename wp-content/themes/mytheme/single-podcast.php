a9cbd04f55eac542dd626f63a925511c57abad08
Jose Hartkopp <josehartkopp@Joses-MacBook-Pro.local>
2021-04-27 16:24:56 +0200

    single podcast indhentning af episoder

--- wp-content/themes/mytheme/single-podcast.php
+++ wp-content/themes/mytheme/single-podcast.php
@@ -1,12 +1,23 @@
 <?php mesmerize_get_header(); ?>
-<button class="back">Tilbage</button>
-<h1 class="podcast_navn">Podcasten</h1>
-<article id="podcast">
-	<img src="" alt="" class="podcast_billede">
-	<p class="beskrivelse"></p>
-</article>
+<main id="main_podcast_single">
+	<button class="back">Tilbage</button>
+	<h1 class="podcast_navn">Podcasten</h1>
+	<article id="podcast">
+		<div class="top_pocast"><img src="" alt="" class="main_podcast_billede">
+			<div class="ikoner">
+				<img src="some_ikon/splogo.png" class="spotify, lyt_ikon" alt="spotify ikon">
+				<img src="some_ikon/applepodcast.png" class="apple, lyt_ikon" alt="apple podcast ikon">
+				<img src="some_ikon/podimo.png" class="podimo, lyt_ikon" alt="podimo ikon">
+				<img src="some_ikon/googlepodcast.png" class="google, lyt_ikon" alt="google podcast ikon">
+			</div>
+		</div>
+		<div class="bottom_podcast">
+			<p class="beskrivelse"></p>
+			<button>AFSPIL</button>
+		</div>
+	</article>
 
-<!-- Tilføj evt. senere nyeste afsnit
+	<!-- Tilføj evt. senere nyeste afsnit
 <article id="ny_podcast">
 	<h2>Nyeste episode</h2>
 	<div class="nyeste_container">
@@ -19,23 +30,105 @@
 </article>
 -->
 
-<h2>Alle Episoder</h2>
-<section class="episode_section">
-</section>
+	<h2>Alle Episoder</h2>
+	<section class="episode_section">
+	</section>
 
-<template>
-	<img src="" alt="" class="podcast_billede">
-	<div>
-		<h3 class="episode_navn"></h3>
-		<p class="episode_tekst"><a href="">Læs mere</a></p>
-		<!--		<a href="">Læs mere</a>-->
-	</div>
-</template>
+	<template>
+		<div class="episode_container">
+			<img src="" alt="" class="podcast_billede">
+			<div class="episode_fakta">
+				<h3 class="episode_navn"></h3>
+				<p class="episode_tekst"></p>
+				<div class="mere_button"></div>
+			</div>
+		</div>
+	</template>
+</main>
 
 <style>
-	.back {
-		padding: 0.3rem;
-		background-color: #E4254A;
+	main {
+		max-width: 1200px;
+		margin: 0 auto;
+		padding-left: 10px;
+		padding-right: 10px;
+	}
+
+	.episode_container {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		grid-gap: 20px;
+		margin-bottom: 15px;
+		background-color: #F5F5F5;
+		padding-top: 10px;
+		padding-bottom: 10px;
+	}
+
+	.episode_container:before {
+		display: none;
+	}
+
+	.episode_container:after {
+		display: none;
+	}
+
+	.beskrivelse {
+		padding: 10px;
+		text-align: left;
+	}
+
+	.podcast_billede {
+		max-width: 100%;
+		margin: 5px;
+	}
+
+	.main_podcast_billede {
+		max-width: 100%;
+	}
+
+	.episode_navn {
+		text-align: left;
+	}
+
+	.episode_tekst {
+		text-align: left;
+	}
+
+	/*TABLET*/
+	@media only screen and (max-width: 800px) {}
+
+
+	/*Desktop*/
+	@media only screen and (min-width: 992px) {
+		main {
+			max-width: 1200px;
+			margin: 0 auto;
+			padding-left: 30px;
+			padding-right: 30px;
+		}
+
+		#podcast {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			grid-gap: 2rem;
+			margin-bottom: 15px;
+		}
+
+		#podcast:before {
+			display: none;
+		}
+
+		#podcast:after {
+			display: none;
+		}
+
+		.main_podcast_billede {
+			max-width: 85%;
+		}
+
+		.podcast_billede {
+			max-width: 50%;
+		}
 	}
 
 </style>
@@ -65,9 +158,9 @@
 
 	function visPodcast() {
 		console.log("visPodcasts");
-		document.querySelector(".podcast_billede").src = podcast.billede.guid;
-		document.querySelector(".podcast_billede").alt = podcast.title.rendered;
-		document.querySelector(".podcast_billede").title = podcast.title.rendered;
+		document.querySelector(".main_podcast_billede").src = podcast.billede.guid;
+		document.querySelector(".main_podcast_billede").alt = podcast.title.rendered;
+		document.querySelector(".main_podcast_billede").title = podcast.title.rendered;
 
 		document.querySelector("h1").innerHTML = podcast.title.rendered;
 		document.querySelector(".beskrivelse").innerHTML = podcast.podcast_beskrivelse;
@@ -82,7 +175,6 @@
 	//		document.querySelector(".ny_episode").innerHTML = episode.title.rendered;
 	//		document.querySelector(".ny_tekst").innerHTML = episode.title.rendered;}}
 
-	getJson()
 
 	function visEpisoder() {
 		console.log("visEpisoder");
@@ -96,13 +188,17 @@
 				console.log("loop kører id :" +
 					aktuelPodcast);
 				let klon = template.cloneNode(true);
+
+				klon.querySelector(".episode_container").setAttribute("id", `pid_${podcast.id}`);
 				klon.querySelector(".podcast_billede").src = episode.podcast_billede.guid;
 				klon.querySelector(".podcast_billede").alt = episode.podcast_billede.guid;
 				klon.querySelector(".podcast_billede").title = episode.podcast_billede.guid;
 
 				klon.querySelector(".episode_navn").innerHTML = episode.episode_navn;
 
-				klon.querySelector(".episode_tekst").innerHTML = episode.episode_beskrivelse;
+				klon.querySelector(".episode_tekst").innerHTML = episode.episode_beskrivelse.substring(0, 90) + "...";
+				klon.querySelector(".mere_button").innerHTML += `<button>Læs mere</button>`
+				klon.querySelector(".mere_button").addEventListener("click", () => visMere(episode));
 
 				sektion.appendChild(klon);
 
@@ -111,6 +207,32 @@
 		document.querySelector("button").addEventListener("click", tilbageTilMenu);
 
 	}
+
+	//-------VIS MERE KNAP-------
+	function visMere(episode) {
+		console.log("click");
+
+		document.querySelector(".mere_button").removeEventListener("click", () => visMere(episode));
+
+		document.querySelector(`#pid_${podcast.id}`).querySelector(".episode_tekst").innerHTML = episode.episode_beskrivelse;
+
+		document.querySelector(`#pid_${podcast.id}`).querySelector(".mere_button").innerHTML = `<button>Læs mindre</button>`
+
+
+		document.querySelector(`#pid_${podcast.id}`).querySelector(".mere_button").addEventListener("click", () => visMindre(episode));
+
+	}
+
+	//-------VIS MINDRE KNAP--------
+	function visMindre(episode) {
+		console.log(episode);
+		document.querySelector(`#pid_${podcast.id}`).querySelector(".episode_tekst").textContent = episode.episode_beskrivelse.substring(0, 90) + "...";
+
+		document.querySelector(`#pid_${podcast.id}`).querySelector(".mere_button").innerHTML = `<button>Læs mere</button>`
+
+		document.querySelector(`#pid_${podcast.id}`).querySelector(".mere_button").addEventListener("click", () => visMere(episode));
+	}
+
 
 	function tilbageTilMenu() {
 		history.back();
