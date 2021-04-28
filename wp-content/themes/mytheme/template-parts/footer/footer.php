c6cec582f2f32e1098cb61b279113ae426ae7fa3
emil7809 <43147664+emil7809@users.noreply.github.com>
2021-04-28 13:21:41 +0200

    emilys footer opdateret

--- wp-content/themes/mytheme/template-parts/footer/footer.php
+++ wp-content/themes/mytheme/template-parts/footer/footer.php
@@ -4,24 +4,33 @@
     <!--            <div class="row middle-xs footer-content-row">-->
     <!--                <div class="footer-content-col col-xs-12">-->
     <div class="info_container">
-        <div>
-            <p>Radio LOUD</p>
-            <p>Kontakt@radioloud.dk
-                <br>+45 32 42 17 14 </p>
-            <p>Programredaktion</p>
-            <p>Wildersgade 10B, 2. sal
-                <br>1408 København</p>
-            <p>Nyhedsredaktion</p>
-            <p>Vestergade 165D, 2.sal
-                <br> 5700 Svendborg</p>
-        </div>
         <div class="om_loud">
-            <p>Om radio LOUD</p>
-            <a href="">Mere LOUD</a>
-            <a href="">Presse</a>
-            <a href="">Job</a>
-            <a href="">Praktikant på LOUD</a>
+            <p class="bold mb">Om radio LOUD</p>
+            <a href="">Mere LOUD</a><br>
+            <a href="">Presse</a><br>
+            <a href="">Job</a><br>
+            <a href="">Praktikant på LOUD</a><br>
+            <div class="some">
+                <img src="wp-content/themes/mytheme/template-parts/footer/facebook.png" alt="facebook">
+                <img src="wp-content/themes/mytheme/template-parts/footer/instagram.png" alt="facebook">
+                <img src="wp-content/themes/mytheme/template-parts/footer/youtube.png" alt="facebook">
+            </div>
         </div>
+        <div class="kontakt">
+            <p class="bold">Radio LOUD</p>
+            <p>Kontakt@radioloud.dk
+                <br>+45 32 42 17 14
+            </p>
+            <p class="bold">Programredaktion</p>
+            <p>Wildersgade 10B, 2. sal
+                <br>1408 København
+            </p>
+            <p class="bold">Nyhedsredaktion</p>
+            <p>Vestergade 165D, 2.sal
+                <br> 5700 Svendborg
+            </p>
+        </div>
+
 
         <!--                </div>-->
         <!--            </div>-->
@@ -31,22 +40,67 @@
 
 
 <style>
+    .gridContainer {
+        width: 100%;
+        max-width: 100%;
+        background-color: #1f2338;
+        padding: 30px;
+
+    }
+
     .info_container {
-        display: flex;
-        flex-direction: row;
-    }
-
-    .footer a {
-        text-decoration: none;
-        color: #9a9a9a;
-    }
-
-    .footer a:hover {
-        color: #e26540 !important;
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        margin-left: 50px;
+        margin-right: 50px;
+        grid-gap: 20px;
+        color: white;
     }
 
     .om_loud {
-        display: block;
+
+        grid-column-start: 3;
+        grid-column-end: span 1;
+        margin: 0 auto;
+    }
+
+    .some img {
+
+        width: 3vw;
+        height: 3vw;
+    }
+
+    .some {
+        margin-left: -0.9vw;
+
+    }
+
+
+    .kontakt {
+
+        grid-column-start: 4;
+        grid-column-end: span 1;
+        margin: 0 auto;
+
+    }
+
+    .om_loud a {
+        text-decoration: none;
+        color: white;
+    }
+
+    .om_loud a:hover {
+        color: #e4254a;
+    }
+
+    .bold {
+        font-weight: 1000;
+        font-size: 1.3rem;
+    }
+
+    .mb {
+
+        margin-bottom: 1px;
     }
 
 </style>
